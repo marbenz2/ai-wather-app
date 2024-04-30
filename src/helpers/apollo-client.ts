5f863c0c8df06d544633d7487f8c8883a26389dc
marbenz2 <benzinger.marcel@gmail.com>
2024-04-30 22:15:45 +0200

    fix API_URI for production build

--- src/helpers/apollo-client.ts
+++ src/helpers/apollo-client.ts
@@ -3,17 +3,17 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import config from "../../stepzen/stepzen.config.json";
 const endpoint = config.endpoint;
 
-let SERVER_URI;
+let API_URI;
 
 if (process.env.NODE_ENV === "development") {
-  SERVER_URI = process.env.LOCAL_URL + endpoint;
+  API_URI = process.env.LOCAL_URL + endpoint;
 } else {
-  SERVER_URI = process.env.PUBLIC_URL + endpoint;
+  API_URI = process.env.PUBLIC_URL + endpoint + "__graphql";
 }
 
 export const getClient = () => {
   const client = new ApolloClient({
-    uri: SERVER_URI,
+    uri: API_URI,
     cache: new InMemoryCache(),
     headers: {
       Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
