d21ee915dacbd130c755386967694934dbfd806b
marbenz2 <benzinger.marcel@gmail.com>
2024-05-23 02:20:58 +0200

    style adjusted the padding for better mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import CityPicker from "@/components/CityPicker";
-import { Card, Divider, Text } from "@tremor/react";
+import { Card, Text } from "@tremor/react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-10 flex flex-col justify-center items-center bg-tremor-background-strong">
+    <div className="min-h-screen p-2 md:p-10 flex flex-col justify-center items-center bg-tremor-background-strong">
       <div className="w-full max-w-2xl mx-auto">
         <Card
           decoration="top"
           decorationColor="amber"
-          className="flex flex-col gap-8"
+          className="flex flex-col gap-8 p-2"
         >
           <Text className="!text-4xl md:!text-6xl font-thin text-center">
             KI Wetter App
           </Text>
-          <Card>
+          <Card className="p-2">
             <CityPicker />
           </Card>
         </Card>
