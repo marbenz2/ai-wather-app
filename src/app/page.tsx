8890284c7e34632f4942b83738e9e42db0bf03a7
marbenz2 <benzinger.marcel@gmail.com>
2024-05-23 02:26:31 +0200

    sytle adjusted padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-2 md:p-10 flex flex-col justify-center items-center bg-tremor-background-strong">
+    <div className="min-h-screen px-2 md:px-10 flex flex-col justify-center items-center bg-tremor-background-strong">
       <div className="w-full max-w-2xl mx-auto">
         <Card
           decoration="top"
           decorationColor="amber"
-          className="flex flex-col gap-8 p-2"
+          className="flex flex-col gap-8 px-2 py-4"
         >
           <Text className="!text-4xl md:!text-6xl font-thin text-center">
             KI Wetter App
           </Text>
-          <Card className="p-2">
+          <Card className="px-2 py-4">
             <CityPicker />
           </Card>
         </Card>
